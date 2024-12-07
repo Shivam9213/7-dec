2a5aba2659901852cbd7380f39f461ec6c5171cd
Shivam9213 <166914102+Shivam9213@users.noreply.github.com>
2024-12-07 15:45:00 +0000

    data types

---
+++ 1_basic/1_bsics.js
@@ -0,0 +1,8 @@
+const accountID=475877
+let accountmail="shiva@hgmail"
+var accountnum="shibva"
+accountcity="jaipur"
+//accountID=455666
+//console.log(accountID);
+console.table([accountmail,accountnum,accountcity,accountID]);
+
---
+++ 1_basic/2basic.js
@@ -0,0 +1,3 @@
+"use strict";  //treat all js code for nre version
+//alert(9+9)  not work here because we work on node.js not on browser
+console.log(typeof null);
