2d7f35bfd5f04090a9b99a28d152a04580f74749
Shivam9213 <166914102+Shivam9213@users.noreply.github.com>
2024-12-09 09:23:50 +0000

    datatypesummay

--- 1_basic/2basic.js
+++ 1_basic/2basic.js
@@ -1,3 +1,19 @@
 "use strict";  //treat all js code for nre version
 //alert(9+9)  not work here because we work on node.js not on browser
 console.log(typeof null);
+
+//array
+const arr=["shivam","maurya","33"]
+
+// object 
+ 
+const obj={
+    name:"shivam",
+    age:22,
+}
+
+// function 
+let functionname= function(){
+    console.log("harry putter");
+    
+}
