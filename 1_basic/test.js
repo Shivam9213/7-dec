d76754bf432c0ea1860cecbfa69709dfd4620df6
Shivam9213 <166914102+Shivam9213@users.noreply.github.com>
2024-12-25 10:21:52 +0000

    okk

--- 1_basic/test.js
+++ 1_basic/test.js
@@ -1,1 +1,50 @@
-console.log("shivam maurya")
+// console.log("shivam maurya")
+// function addone(num){
+//     return num+1
+// }
+
+// const addtwo=function(num){
+//     return num+2
+// }
+
+// //console.log(addone(1));
+// //console.log(addtwo(1));
+// const  obj={
+//     name:"shibvm",
+//     age:44,
+//     welcome:function(){
+//         console.log(`${this.name},welcome to you`);
+//         console.log(this);   
+//     }
+// }
+// // obj.welcome()
+// // obj.name="jnf"
+// // obj.welcome()
+
+// function oo(){
+//     console.log(this);
+    
+// }
+// //oo()
+// const chai=()=>{
+//     const name="shivam"
+//     console.log(this.name);
+    
+// }
+// chai()
+const addd=(num,num1)=>  (num+num1)
+
+//console.log(addd(3,4));
+//IIRE (imiditely invokw function execution)
+(function cc(){
+    console.log('DB connected');
+    
+})();
+(()=>{
+    console.log('kjgnrkjng');
+    
+})();
+((name)=>{
+    console.log(`kjgnrkjng ${name}`);
+    
+})("shivam");
